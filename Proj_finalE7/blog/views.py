bd3d776f3954e66484700d7c00dcf30b916bb762
llalexis90ll <alexis.peloso@gmail.com>
2022-08-05 18:36:51 -0300

    creado modelo post y articulodetallado y funcionamiento de url

--- Proj_finalE7/blog/views.py
+++ Proj_finalE7/blog/views.py
@@ -1,23 +1,37 @@
 from http.client import HTTPResponse
 from django.shortcuts import render , HttpResponse # el http no hay q usar
 
+
+from django.views.generic import ListView, DetailView
+from .models import Post
+
 # Create your views here.
-def mi_vista(request):
-    return HttpResponse("<h1>IVAN XD</h1><h2>HOLAAaa</h2> ")
+#def mi_vista(request):
+#    return HttpResponse("<h1>IVAN XD</h1><h2>HOLAAaa</h2> ")
 
 #pruebas con html url /documento
-def documento (request):
-    documento = """
-    <html>
-    <body>
-    <h1>
-    DOCUMENTO PRIMERA PAGINA 
-    </h1>
-    <h2 stylo = "color: red">
-    Pruebas de html
-    </h2>
-    </body>
-    </html>
+# def documento (request):
+#     documento = """
+#     <html>
+#     <body>
+#     <h1>
+#     DOCUMENTO PRIMERA PAGINA 
+#     </h1>
+#     <h2 stylo = "color: red">
+#     Pruebas de html
+#     </h2>
+#     </body>
+#     </html>
 
-    """ 
-    return HttpResponse(documento)
+#     """ 
+#     return HttpResponse(documento)
+
+
+class Inicio(ListView):
+    model = Post
+    template_name = 'inicio.html'
+
+
+class ArticuloDetalle(DetailView):
+    model = Post
+    template_name = 'articulo_detalle.html'

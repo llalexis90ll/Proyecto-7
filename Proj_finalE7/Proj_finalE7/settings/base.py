4f850f6916ad04ad69bbd448cd0cae8af9fe007e
llalexis90ll <alexis.peloso@gmail.com>
2022-08-15 22:30:26 -0300

    prueba postgresql

--- Proj_finalE7/Proj_finalE7/settings/base.py
+++ Proj_finalE7/Proj_finalE7/settings/base.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'Proj_finalE7.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bloog',
+        'USER': 'postgres',
+        'PASSWORD': 'peloso2132',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- Proj_finalE7/Proj_finalE7/settings/local.py
+++ Proj_finalE7/Proj_finalE7/settings/local.py
@@ -1,1 +1,4 @@
 from .base import *
+
+
+ALLOWED_HOSTS = ['127.0.0.1']

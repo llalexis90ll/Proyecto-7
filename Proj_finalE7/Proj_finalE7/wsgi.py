de9efc132fc80eef8d2581c3751f64603dce1904
llalexis90ll <alexis.peloso@gmail.com>
2022-08-25 18:59:12 -0300

    wsgi.py

--- Proj_finalE7/Proj_finalE7/wsgi.py
+++ Proj_finalE7/Proj_finalE7/wsgi.py
@@ -14,7 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Proj_finalE7.settings')
 
 application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise  
-
-application = DjangoWhiteNoise(application)
